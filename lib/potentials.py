57083690cb22d4512eedd87897d6a6d5631dd290
Nadine Bradbury <nadinebradbury@gmail.com>
2025-09-30 15:41:21 -0400

    extents factory for erf_coulomb update

--- lib/potentials.py
+++ lib/potentials.py
@@ -60,7 +60,7 @@ extents_soft_coulomb = _extents_log_factory(
     [8,   5,   4,   3.5, 3.5]
 )
 
-def erf_coulomb(R, r1e, r2e, charges, alpha=2, beta=2):
+def erf_coulomb(R, r1e, r2e, charges, alpha=6, beta=1):
     Q1,Q2 = charges
     V1  = -Q1 *      erf(alpha*r1e) / r1e
     V2  = -Q2 *      erf(alpha*r2e) / r2e
@@ -69,9 +69,9 @@ def erf_coulomb(R, r1e, r2e, charges, alpha=2, beta=2):
     return V1 + V2 + VN
 
 extents_erf_coulomb = _extents_log_factory(
-    [1e1, 1e2, 1e3, 1e4, 1e5],
-    [0.1, 0.1, 0.3, 0.3,   0.3],
-    [8,   4,   3,   2.5, 2.5]
+    [2e1, 2e2, 2e3, 2e4, 2e5],
+    [0.1, 0.2, 0.5, 0.5,   1],
+    [20,   10,   5,   3.5, 3]
 )
 
 def soft_coulomb_barrier(R, r1e, r2e, charges, dv=0.5, G=1, p=2, A=1):
