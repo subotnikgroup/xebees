16c714383d2fa6748d1d82d6b97508fead0c8d0e
D. Vale Cofer-Shabica <vale@princeton.edu>
2025-11-20 16:26:42 -0500

    fix Om projection in spinful case

--- lib/analysis.py
+++ lib/analysis.py
@@ -16,18 +16,14 @@ def get_wfc_proj(evecs, H):
 
 def get_wfc_Om_proj_wS(evecs, H):
     #### symbols are currently meaningless!!!! Need to rethink how we want to describe Om for half int J!
-    symb = ['s','s','p','d'] + [chr(c) for c in range(ord('f'), ord('z')+1)]
-    J = int(H.J-0.5)
+    symb = ['s','p','d'] + [chr(c) for c in range(ord('f'), ord('z')+1)]
+    J = int(H.J+0.5)
 
     ev = evecs.reshape((-1,) + H.shape)
     prj = xp.sum(ev**2, axis=(1,2,3,4))
-    if J==0:
-        symbols = ['s' for _,_ in enumerate(evecs)]
-        return symbols, prj
-    
-    d = prj[: ,J:].copy() # 0..J
-    #d[:, 1:] += prj[:, :J][:, ::-1]  # -J..-1 with flip along 2nd axis (J) # not supported by cupynumeric
-    d[:, 1:] += xp.fliplr(prj[:, :J]) # -J..-1 with flip along 2nd axis (J)
+
+    d = prj[: ,J:].copy() # 1/2..J
+    d += xp.fliplr(prj[:, :J]) # -J..-1/2 with flip along 2nd axis (J)
 
     symbols = list(map(lambda x: symb[int(xp.argmax(x))], d))
     return symbols, prj
