a6e7512282a2c46a5f73018a9b1256eed7078bfe
BhatiMansi <mansibhati0599@gmail.com>
2026-03-24 15:05:55 -0400

    gpu 1D code

---
+++ 1D/exact_gpu.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+import numpy
+from sys import stderr
+import argparse as ap
+from pathlib import Path
+from threadpoolctl import ThreadpoolController
+
+import os, sys
+sys.path.append(os.path.abspath("lib"))
+
+import xp
+from constants import *
+from hamiltonian import  KE, KE_FFT
+from davidson import solve_davidson, solve_exact, get_davidson_guess
+from debug import prms, timer
+import potentials
+
+
+def VO(R_amu, r_amu, g_1, g_2, M_1, M_2):
+    R, r = R_amu / ANGSTROM_TO_BOHR, r_amu / ANGSTROM_TO_BOHR
+    D, d, a, c = 60, 0.95, 2.52, 1
+    A, B, C = 2.32e5, 3.15, 2.31e4
+    mu = M_1*M_2/(M_1+M_2)
+
+    D1 = g_2 * D * (    xp.exp(-2*a * (r + mu/M_2*R - d))
+                    - 2*xp.exp(  -a * (r + mu/M_2*R - d))
+                    + 1)
+    D2 = g_1 * D * c**2 * (    xp.exp(-(2*a / c) * (mu/M_1*R - r - d))
+                           - 2*xp.exp(-(  a / c) * (mu/M_1*R - r - d)))
+    VN = g_1 * g_2 * (A * xp.exp(-B * R) - C / R**6)
+
+    return KCALMOLE_TO_HARTREE * (D1 + D2 + VN)
+
+
+
+
+def parse_args():
+    parser = ap.ArgumentParser(
+        prog='davidson-ps-1d',
+        description="computes the lowest k eigenvalues of phase space model in Xuezhi's paper")
+
+    parser.add_argument('-k', metavar='num_eigenvalues', default=5, type=int)
+    parser.add_argument('-t', metavar="num_threads", default=16, type=int)
+    parser.add_argument('-g_1', metavar='g_1', required=True, type=float)
+    parser.add_argument('-g_2', metavar='g_2', required=True, type=float)
+    parser.add_argument('-M_1', required=True, type=float)
+    parser.add_argument('-M_2', required=True, type=float)
+    parser.add_argument('-R', dest="NR", metavar="NR", default=101, type=int)
+    parser.add_argument('-r', dest="Nr", metavar="Nr", default=400, type=int)
+    parser.add_argument('--exact_diagonalization', action='store_true')
+    parser.add_argument('--verbosity', default=2, type=int)
+    parser.add_argument('--backend', default='numpy')
+    parser.add_argument('--iterations', metavar='max_iterations', default=10000, type=int)
+    parser.add_argument('--subspace', metavar='max_subspace', default=1000, type=int)
+    parser.add_argument('--guess', metavar="guess.npz", type=Path, default=None)
+    parser.add_argument('--evecs', metavar="guess.npz", type=Path, default=None)
+    parser.add_argument('--save', metavar="filename")
+
+    return parser.parse_args()
+
+
+def build_terms(args):
+    m = AMU_TO_AU * 1
+    M_1 = AMU_TO_AU * args.M_1
+    M_2 = AMU_TO_AU * args.M_2
+    mu = M_1*M_2/(M_1+M_2)
+
+    extent = potentials.extents_borgis(mu)
+    print("extent",extent)
+    R_min = extent[0]
+    R_max = extent[1]
+    r_min = -extent[2]
+    r_max = extent[2]
+
+
+    R = xp.linspace(R_min, R_max, args.NR)
+    r = xp.linspace(r_min, r_max, args.Nr)
+
+    dR, dr = R[1] - R[0], r[1] - r[0]
+    Vgrid = VO(*xp.meshgrid(R, r, indexing='ij'), args.g_1, args.g_2, M_1, M_2)
+
+    P = xp.fft.fftshift(xp.fft.fftfreq(args.NR, dR)) * 2 * xp.pi
+    p = xp.fft.fftshift(xp.fft.fftfreq(args.Nr, dr)) * 2 * xp.pi
+
+    
+    Tr = KE(args.Nr, dr, m)
+    Tmp = KE(args.Nr, dr, (M_1+M_2))
+    TR = -xp.real(KE_FFT(args.NR, P, R)) / (2 * mu)
+
+    return dr, TR, Tr, Tmp, Vgrid, (R,P), (r,p)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    print(args)
+
+    # you can only select the backend once and it must be before you use any xp functions
+    if xp.backend != args.backend:
+        xp.backend = args.backend
+
+    # set number of threads for Davidson etc.
+    threadctl = ThreadpoolController()
+    threadctl.limit(limits=args.t)
+
+    batch_eigvalsh = xp.linalg.eigvalsh
+    if xp.backend == 'cupy':
+        try:
+            print("cupy detected; trying diagonalization with torch backend")
+            import torch
+            torch.cuda.current_device()
+        except ModuleNotFoundError:
+            print("torch not found.")
+        except AssertionError:
+            print("torch not available.")
+        else:
+            def torch_eigvalsh(H):
+                return xp.asarray(torch.linalg.eigvalsh(torch.from_dlpack(H)))
+            batch_eigvalsh = torch_eigvalsh 
+
+    dr,TR, Tr, Tmp, Vgrid, *_ = build_terms(args)
+
+    # load a guess if there is one
+    davidson_guess = get_davidson_guess(args.guess, (args.NR, args.Nr))
+    conv, e_approx, evecs = solve_davidson(TR, Tr + Tmp, Vgrid,
+                                           num_state=args.k,
+                                           verbosity=args.verbosity,
+                                           iterations=args.iterations,
+                                           max_subspace=args.subspace,
+                                           guess=davidson_guess,
+    )
+    print("Davidson:", e_approx)
+    print(conv)
+    p2 = KE(args.Nr, dr,order=2, bare=True)
+    print("evecs[0].shape",evecs[0].shape)
+    print("p2.shape",p2.shape)
+    evecs_0 = evecs[0].reshape(args.NR, args.Nr)
+    p2_expec = -1j*xp.einsum('Rr, rx, Rx -> ', xp.conj(evecs_0), p2, evecs_0)
+    #p2_expec = xp.conj(evecs[0]).T @ (p2 @ evecs[0])
+    print("p2_expec",p2_expec)
+    if args.evecs:
+        numpy.savez(args.evecs, guess=evecs, V=Vgrid)
+        print("Wrote eigenvectors to", args.evecs)
+
+
+    if args.save is not None:
+        if all(conv):
+            with open(args.save, "a") as f:
+                print(args.M_1, args.M_2, " ".join(map(str, e_approx)), file=f)
+            print(f"Computed fixed center-of-mass eigenvalues",
+                  f"for M_1={args.M_1}, M_2={args.M_2} amu",
+                  f"with charges g_1={args.g_1}, g_1={args.g_1}",
+                  f"and appended to {args.save}")
+        else:
+            print("Skipping saving unconverged results.")
+
+    if args.exact_diagonalization:
+        e_exact = solve_exact(TR, Tr + Tmp, Vgrid, num_state=args.k)
+        print("Exact:", e_exact)
+        prms(e_approx, e_exact, "RMS deviation between Davidson and Exact")
+
+    if not all(conv):
+        print("WARNING: Not all eigenvalues converged")
+        exit(1)
+    else:
+        print("All eigenvalues converged")
---
+++ 1D/gpu_ps.py
@@ -0,0 +1,378 @@
+import sys
+import fcntl
+#import numpy as xp
+
+import os, sys
+sys.path.append(os.path.abspath("lib"))
+from hamiltonian import  inverse_weyl_transform
+import potentials
+from constants import *
+
+import xp
+import math
+
+# Constants
+amu_to_au = 1822.888486209
+angstrom_to_bohr = 1.8897259886
+
+def VO(R_au, r1e_au, r2e_au, charges):
+    """
+    Potential energy in Hartree.
+    """
+
+    Q1, Q2 = charges
+
+    R   = R_au   / ANGSTROM_TO_BOHR
+    r1e = r1e_au / ANGSTROM_TO_BOHR
+    r2e = r2e_au / ANGSTROM_TO_BOHR
+
+    D, d, a, c = 60, 0.95, 2.52, 1
+    A, B, C = 2.32e5, 3.15, 2.31e4
+
+    D2 = Q2 * D * (     xp.exp(-2*a * (r2e-d))
+                    - 2*xp.exp(  -a * (r2e-d))
+                    + 1)
+    D1 = Q1 * D * c**2 * (     xp.exp(-(2*a/c) * (r1e-d))
+                           - 2*xp.exp(-(  a/c) * (r1e-d)))
+
+    VN = Q1 * Q2 * (A*xp.exp(-B*R) - C/R**6)
+
+    return 0.00159362 * (D1 + D2 + VN)
+
+def get_stencil_coefficients(stencil_size, derivative_order):
+    """
+    Get finite-difference stencil coefficients
+    """
+    if stencil_size % 2 == 0:
+        raise ValueError("Stencil size must be odd.")
+    
+    half_size = stencil_size // 2
+    A = xp.zeros((stencil_size, stencil_size))
+
+    x_vals = xp.arange(-half_size, half_size + 1)
+    for i in range(stencil_size):
+        A[i] = x_vals**i
+
+    b = xp.zeros(stencil_size)
+    b[derivative_order] = math.factorial(derivative_order)
+
+    # Solve for coefficients
+    return xp.linalg.solve(A, b)
+
+def KE(n, dx, mass, stencil_size=7):
+    """
+    Kinetic energy operator
+    """
+    stencil = get_stencil_coefficients(stencil_size, 2) / dx**2
+    I = xp.eye(n)
+    # Build T by convolving each basis vector with the stencil
+    T = xp.array([xp.convolve(I[i], stencil, mode='same') for i in range(n)])
+    T *= -1.0 / (2.0 * mass)
+    return T
+
+def PO(n, r, stencil_size=7):
+    """
+    First-order momentum operator 
+    """
+    dr = r[1] - r[0]
+    stencil = get_stencil_coefficients(stencil_size, 1) / dr
+    I = xp.eye(n)
+    pe = xp.array([xp.convolve(I[i], stencil, mode='same') for i in range(n)], dtype=complex)
+    return -1j * pe
+
+def PO2(n, r, stencil_size=7):
+    """
+    Second-order momentum operator (p^2 -> -d^2/dx^2).
+    """
+    dr = r[1] - r[0]
+    stencil = get_stencil_coefficients(stencil_size, 2) / dr**2
+    I = xp.eye(n)
+    pe2 = xp.array([xp.convolve(I[i], stencil, mode='same') for i in range(n)], dtype=complex)
+    return -pe2
+
+def Gamma(r, R_val, pe, M1, M2, sigma=1, w=1):
+    """
+    Gamma operator
+    """
+    mu12 = M1*M2/(M1+M2)
+    #r1e2, r2e2 = H.V(H.R[i], H.x_grid, H.y_grid, H.z_grid, spitvals=True)
+    kappa2 = r*R_val
+    r1e2 = r**2 + (R_val)**2*(mu12/M1)**2 - 2*kappa2*mu12/M1
+    r2e2 = r**2 + (R_val)**2*(mu12/M2)**2 + 2*kappa2*mu12/M2
+    theta1 = xp.exp(-r1e2)
+    theta2 = xp.exp(-r2e2)
+    partition = theta1 + theta2
+    t1 = xp.diag(theta1 / partition)
+    t2 = xp.diag(theta2 / partition)
+
+    gamma1 = (t1 @ pe + pe @ t1) / (2j)
+    gamma2 = (t2 @ pe + pe @ t2) / (2j)
+    return (M2*gamma1 - M1*gamma2) / (M1 + M2)
+
+def Gamma2(r, R_val, pe, pe2, M1, M2, sigma=1, w=1):
+    """
+    Second-order Gamma operator
+    """
+
+    kappa2 = r*R_val
+    r1e2 = r**2 + (R_val)**2*(mu12/M1)**2 - 2*kappa2*mu12/M1
+    r2e2 = r**2 + (R_val)**2*(mu12/M2)**2 + 2*kappa2*mu12/M2
+    theta1 = xp.exp(-r1e2)
+    theta2 = xp.exp(-r2e2)
+    partition = theta1 + theta2
+    t1 = theta1 / partition
+    t2 = theta2 / partition
+
+    def get_gamma2(a, b, op1, op2):
+        da = xp.gradient(a, r)
+        db = xp.gradient(b, r)
+        d2b = xp.gradient(db, r)
+        da_diag = xp.diag(da)
+        db_diag = xp.diag(db)
+        d2b_diag = xp.diag(d2b)
+        a_diag = xp.diag(a)
+        b_diag = xp.diag(b)
+
+        a1 = -1j * a_diag @ db_diag @ op1 + a_diag @ b_diag @ op2
+        a2 = -a_diag @ d2b_diag - 2j * a_diag @ db_diag @ op1 + a_diag @ b_diag @ op2
+        a3 = -1j * da_diag @ b_diag @ op1 - 1j * a_diag @ db_diag @ op1 + a_diag @ b_diag @ op2
+        a4 = (-da_diag @ db_diag - 1j * da_diag @ b_diag @ op1 - a_diag @ d2b_diag
+              - 2j * a_diag @ db_diag @ op1 + a_diag @ b_diag @ op2)
+
+        return -0.25 * (a1 + a2 + a3 + a4)
+
+    gamma11 = get_gamma2(t1, t1, pe, pe2)
+    gamma12 = get_gamma2(t1, t2, pe, pe2)
+    gamma21 = get_gamma2(t2, t1, pe, pe2)
+    gamma22 = get_gamma2(t2, t2, pe, pe2)
+    return (M2**2*gamma11 - M1*M2*gamma12 - M1*M2*gamma21 + M2**2*gamma22) / (M1 + M2)**2
+
+#def Gammasq(r, R_val, pe, pe2, M1, M2, sigma=1, w=1):
+#    """
+#    Second-order Gamma operator
+#    """
+#    theta1 = w * xp.exp(-((r + R_val/2)**2) / sigma**2)
+#    theta2 = xp.exp(-((r - R_val/2)**2) / sigma**2)
+#    partition = theta1 + theta2
+#    t1 = theta1 / partition
+#    t2 = theta2 / partition
+#    gammma11 = theta1 * xp.exp(-((r + R_val/2)**2) / sigma**2)
+#    gammma22 = 
+#    return (M2**2*gamma11 + M2**2*gamma22) / (M1 + M2)**2
+
+def solve_EPS(NR, Nr, R, r, Mtotal,mu12, mur, M1, M2, P, charges):
+    """
+    Solve for EPS 
+    """
+    EPS = xp.zeros((NR, NR))
+    rPS = xp.zeros((NR, NR), dtype=complex)
+    pPS = xp.zeros((NR, NR), dtype=complex)
+    r2PS = xp.zeros((NR, NR), dtype=complex)
+    p2PS = xp.zeros((NR, NR), dtype=complex)
+    EPSsquare = xp.zeros((NR, NR))
+    pPS_square = xp.zeros((NR, NR), dtype=complex)
+    p2PS_square = xp.zeros((NR, NR), dtype=complex)
+    psiPS_square = xp.zeros((NR, NR, Nr), dtype=complex)
+    piPS_square = xp.zeros((NR, NR), dtype=complex)
+    pi2PS_square = xp.zeros((NR, NR), dtype=complex)
+    EBO = xp.zeros([NR,1])
+    pBO = xp.zeros([NR,1],dtype=complex)
+    p2BO = xp.zeros([NR,1],dtype=complex)
+    sigma = 1
+    g = 1
+    w = 1
+
+
+   
+    dr = r[1] - r[0]
+    ke = KE(Nr, dr, m, stencil_size=7)
+    pe = PO(Nr, r, stencil_size=7)
+    pe2 = PO2(Nr, r, stencil_size=7)
+
+    for i in range(NR):
+        if i % 10 == 0: print("i",i,flush=True)
+        gamma = Gamma(r, R[i], pe, M1, M2, sigma, w)
+        gamma2 = Gamma2(r, R[i], pe, pe2, M1, M2, sigma, w)
+        
+        kappa2 = r*R[i]
+
+        r1e2 = r**2 + (R[i])**2*(mu12/M1)**2 - 2*kappa2*mu12/M1
+        r2e2 = r**2 + (R[i])**2*(mu12/M2)**2 + 2*kappa2*mu12/M2
+
+        r1e = xp.sqrt(xp.where(r1e2 < 0, 0, r1e2))
+        r2e = xp.sqrt(xp.where(r2e2 < 0, 0, r2e2))
+        v_diag = xp.diag(VO(R[i], r1e, r2e, charges))
+
+        Helbo = ke + v_diag
+        valbo, vecbo = xp.linalg.eigh(Helbo)
+        EBO[i] = valbo[0]
+        #print("EBO",EBO[i],flush=True)
+        gsbo = vecbo[:, 0]
+        pbo = xp.conj(gsbo).T @ (pe @ gsbo)
+        p2bo = xp.conj(gsbo).T @ (pe2 @ gsbo)
+        pBO[i] = pbo
+        p2BO[i] = p2bo
+        
+        for j in range(NR):
+            #print("i,j",i,j,flush=True)
+            Hel_square = ke + v_diag - 1j * gamma * P[j] / mu12 - gamma2 / (2 * mu12)
+            Hel = ke + v_diag - 1j * gamma * P[j] / mu12
+
+            val_square, vec_square = xp.linalg.eigh(Hel_square)
+            val, vec = xp.linalg.eigh(Hel)
+
+            EPSsquare[i, j] = val_square[0] + 0.5 * P[j]**2 / mu12
+            EPS[i,j] = val[0] + 0.5 * P[j]**2 / mu12
+
+            # Expectation values for ground state
+            gs_square = vec_square[:, 0]
+            gs = vec[:, 0]
+            #rPS_square[i, j] = gs_square.conj().T @ xp.diag(r) @ gs_square
+            #r2PS_square[i, j] = gs_square.conj().T @ xp.diag(r**2) @ gs_square
+            #rPS[i, j] = gs.conj().T @ xp.diag(r) @ gs
+            #r2PS[i, j] = gs.conj().T @ xp.diag(r**2) @ gs
+
+            psip_square = xp.fft.fftshift(xp.fft.fft(gs_square)) / xp.sqrt(Nr)
+            psip = xp.fft.fftshift(xp.fft.fft(gs)) / xp.sqrt(Nr)
+
+            p_vals = xp.fft.fftshift(xp.fft.fftfreq(Nr, dr)) * 2.0 * xp.pi
+            pPS_square[i, j] = psip_square.conj().T @ xp.diag(p_vals) @ psip_square
+            pPS[i, j] = psip.conj().T @ xp.diag(p_vals) @ psip
+
+            p2PS_square[i, j] = psip_square.conj().T @ xp.diag(p_vals**2) @ psip_square
+            p2PS[i, j] = psip.conj().T @ xp.diag(p_vals**2) @ psip
+
+            #piPS[i, j] = P[j] - 1j * (gs.conj().T @ gamma @ gs)
+            #pi2PS[i, j] = (P[j]**2 
+            #               - 2j * P[j] * (gs.conj().T @ gamma @ gs)
+            #               - (gs.conj().T @ gamma2 @ gs))
+#
+            #psiPS[i, j] = vec[:, 0]
+
+    KE_BO = KE(NR, dR, mu12, stencil_size=7)
+
+    #return EPS, EPSsquare, pPS, p2PS, pPS_square, p2PS_square
+    return EBO, EPS, EPSsquare, pPS, p2PS, pPS_square, p2PS_square, pBO, p2BO, KE_BO
+
+
+
+if __name__ == "__main__":
+    M1 = float(sys.argv[1]) * amu_to_au
+    M2 = float(sys.argv[2]) * amu_to_au
+    #identifier = sys.argv[2]
+    #sigma_val = float(sys.argv[3])
+    #output_dir = sys.argv[4]  
+    NR = int(sys.argv[3]) + 1
+    Nr = int(sys.argv[4])
+    Q1 = float(sys.argv[5])
+    Q2 = float(sys.argv[6])
+    #g_val = float(sys.argv[7])
+    #w_val = float(sys.argv[8])
+    sigma_val = 1
+    g_val = 1
+    w_val = 1
+    xp.backend = 'cupy'
+
+    m = 1 * amu_to_au        
+    Mtotal = M1 + M2                
+    mu12 = M1 * M2 / (M1 + M2)    
+    mur = (M1 + M2) * m / (M1 + M2 + m)
+
+    extent = potentials.extents_borgis(mu12)
+    print("extent",extent)
+    R_min = extent[0]
+    R_max = extent[1]
+    r_min = -extent[2]
+    r_max = extent[2]
+    R = xp.linspace(R_min, R_max, NR)
+    r = xp.linspace(r_min, r_max, Nr)
+
+    batch_eigvalsh = xp.linalg.eigvalsh
+    if xp.backend == 'cupy':
+        try:
+            print("cupy detected; trying diagonalization with torch backend")
+            import torch
+            torch.cuda.current_device()
+        except ModuleNotFoundError:
+            print("torch not found.")
+        except AssertionError:
+            print("torch not available.")
+        else:
+            def torch_eigvalsh(H):
+                return xp.asarray(torch.linalg.eigvalsh(torch.from_dlpack(H)))
+            batch_eigvalsh = torch_eigvalsh 
+
+    # Masses
+    
+    
+    dR = R[1] - R[0]
+    P = xp.fft.fftshift(xp.fft.fftfreq(NR, dR)) * 2 * xp.pi
+    charges = (Q1, Q2)
+
+    #EPS, EPSsquare, pPS, p2PS, pPS_square, p2PS_square= solve_EPS(NR, Nr, R, r, Mtotal,mu12, mur, M1, M2, P, charges)
+    EBO, EPS, EPSsquare, pPS, p2PS, pPS_square, p2PS_square, pBO, p2BO, KE_BO= solve_EPS(NR, Nr, R, r, Mtotal,mu12, mur, M1, M2, P, charges)
+
+    HPS = inverse_weyl_transform(EPS, NR, R, P)
+    HPSsquare = inverse_weyl_transform(EPSsquare, NR, R, P)
+    EPSv, psiPSv = xp.linalg.eigh(HPS)
+    EPSvsquare, psiPSvsquare = xp.linalg.eigh(HPSsquare)
+    print("EPSv",EPSv[0:10])
+    print("EPSvsquare",EPSvsquare[0:10])
+
+    Hp2PS = inverse_weyl_transform(p2PS, NR, R, P)
+    Hp2PSsquare = inverse_weyl_transform(p2PS_square, NR, R, P)
+    v_p200 = xp.conj(psiPSv[:,0]).T @ (Hp2PS @ psiPSv[:,0])
+    v_p201 = xp.conj(psiPSv[:,1]).T @ (Hp2PS @ psiPSv[:,0])
+    v_p200_square = xp.conj(psiPSvsquare[:,0]).T @ (Hp2PSsquare @ psiPSvsquare[:,0])
+    v_p201_square = xp.conj(psiPSvsquare[:,1]).T @ (Hp2PSsquare @ psiPSvsquare[:,0])
+    print("v_p200",v_p200)   
+    print("v_p200_square",v_p200_square)
+    print("v_p201",v_p201)
+    print("v_p201_square",v_p201_square)
+    
+    HpPS = inverse_weyl_transform(pPS, NR, R, P)
+    HpPSsquare = inverse_weyl_transform(pPS_square, NR, R, P)
+    v_p00 = xp.conj(psiPSv[:,0]).T @ (HpPS @ psiPSv[:,0])
+    v_p01 = xp.conj(psiPSv[:,1]).T @ (HpPS @ psiPSv[:,0])
+    v_p00_square = xp.conj(psiPSvsquare[:,0]).T @ (HpPSsquare @ psiPSvsquare[:,0])
+    v_p01_square = xp.conj(psiPSvsquare[:,1]).T @ (HpPSsquare @ psiPSvsquare[:,0])
+    print("v_p00",v_p00)
+    print("v_p_square",v_p00_square)
+    print("v_p01",v_p01)
+    print("v_p01_square",v_p01_square)
+
+    Rval, Pval = xp.meshgrid(R, P, indexing='ij')
+    EBO_check = xp.zeros((NR, NR))
+    Helmatg = xp.repeat(EBO,NR,axis=1)
+    EBO_check += Helmatg   
+    EBO_check += 1/(2*mu12)*(Pval**2)
+    HBO_check = inverse_weyl_transform(EBO_check, NR, R, P)
+    EBO_weyl_check, vecBO_weyl_check = xp.linalg.eigh(HBO_check)
+
+    #HBO = KE_BO + xp.diag(EBO)
+    #EBO_stencil, vecBO_stencil = xp.linalg.eigh(HBO)
+    #print("EBO",EBO_stencil[0:10])
+    print("EBO_check",EBO_weyl_check[0:10])
+
+    pBORP = xp.repeat(pBO,NR,axis=1)
+    p2BORP = xp.repeat(p2BO,NR,axis=1)
+
+    HpBO = inverse_weyl_transform(pBORP, NR, R, P)
+    Hp2BO = inverse_weyl_transform(p2BORP, NR, R, P)
+    #v_pBO_stencil = xp.conj(vecBO_stencil[:,0]).T @ (HpBO @ vecBO_stencil[:,0])
+    #v_p2BO_stencil = xp.conj(vecBO_stencil[:,0]).T @ (Hp2BO @ vecBO_stencil[:,0])
+
+    v_pBO_check = xp.conj(vecBO_weyl_check[:,0]).T @ (HpBO @ vecBO_weyl_check[:,0])
+    v_p2BO_check = xp.conj(vecBO_weyl_check[:,0]).T @ (Hp2BO @ vecBO_weyl_check[:,0])
+
+    #print("v_pBO",v_pBO_stencil)
+    #print("v_p2BO",v_p2BO_stencil)
+    print("v_pBO_check",v_pBO_check)
+    print("v_p2BO_check",v_p2BO_check)
+
+    
+    #with open(f"EPS_{identifier}.dat", "a") as f:
+    #    fcntl.flock(f, fcntl.LOCK_EX)
+    #    print(M_val, end=' ', file=f)
+    #    print(" ".join(f"{E:.8f}" for E in EPSv), file=f)
+    #    fcntl.flock(f, fcntl.LOCK_UN)
